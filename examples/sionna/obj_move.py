281a81b27c9f134482dff31852554b270803b116
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-03-30 15:07:37 -0300

    (WIP): New XML based move

--- examples/sionna/obj_move.py
+++ examples/sionna/obj_move.py
@@ -1,29 +1,29 @@
 import mitsuba as mi
 import drjit as dr
+import xml.etree.ElementTree as ET
 
 mi.set_variant("cuda_ad_rgb")
 
 
-def getPositionParams(scene):
-    # scene = mi.load_file(mitsuba_file)
+def getPositionParams(mitsuba_file):
+    scene = mi.load_file(mitsuba_file)
     params = mi.traverse(scene)
     return params
 
 
 def translate(
-    scene,
+    mitsuba_file: str,
     obj_name: str,
     x_translation_step: float = 0,
     y_translation_step: float = 0,
     z_translation_step: float = 0,
 ):
-    params = getPositionParams(scene)
-    object = dr.unravel(mi.Point3f, params[f"{obj_name}.vertex_positions"])
-    object.x += x_translation_step
-    object.y += y_translation_step
-    object.z += z_translation_step
-    params[f"{obj_name}.vertex_positions"] = dr.ravel(object)
-    params.update()
+    params = getPositionParams(mitsuba_file)
+    root = ET.parse(mitsuba_file).getroot()
+    # selected_object = root.findall(f"./shape[@id='{obj_name}']/transform/translate")
+    for translation in root.findall(f"./shape[@id='{obj_name}']/transform/translate"):
+        print(translation.attrib)
+    print("END")
 
 
 def moveTo(
@@ -42,5 +42,5 @@ def moveTo(
     params.update()
 
 
-scene = mi.load_file("simple_street_canyon/simple_street_canyon.xml")
-translate(scene, "mesh-Cube", 0, 0, 100)
+mitsuba_file = "/home/joao/codes/caviar/examples/sionna/simple_street_canyon/simple_street_canyon.xml"
+translate(mitsuba_file, "mesh-Cube", -100, 0, 0)
