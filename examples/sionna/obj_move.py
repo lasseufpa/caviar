f1f9e31ab822ba9fdc3cac4b0a23b20e23d3f086
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-03-30 16:07:22 -0300

    Updated translation method to XML approach

--- examples/sionna/obj_move.py
+++ examples/sionna/obj_move.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 mi.set_variant("cuda_ad_rgb")
 
 
-def getPositionParams(mitsuba_file):
+def getParams(mitsuba_file):
     scene = mi.load_file(mitsuba_file)
     params = mi.traverse(scene)
     return params
@@ -18,14 +18,24 @@ def translate(
     y_translation_step: float = 0,
     z_translation_step: float = 0,
 ):
-    params = getPositionParams(mitsuba_file)
-    root = ET.parse(mitsuba_file).getroot()
-    # selected_object = root.findall(f"./shape[@id='{obj_name}']/transform/translate")
-    for translation in root.findall(f"./shape[@id='{obj_name}']/transform/translate"):
-        print(translation.attrib)
-    print("END")
+    xml_tree = ET.parse(mitsuba_file)
+
+    # Get the <translate> grandchildren from a <shape> node with the desired "id"
+    translation_element = (
+        xml_tree.getroot()
+        .findall(f"./shape[@id='{obj_name}']/transform/translate")[0]
+        .attrib
+    )
+    # Update the values in <translate> with the desired translation step
+    translation_element[
+        "value"
+    ] = f"{str(x_translation_step)} {str(y_translation_step)} {str(z_translation_step)}"
+
+    # Write the modifications to disk
+    xml_tree.write(mitsuba_file)
 
 
+# Currently deprecated
 def moveTo(
     mitsuba_file: str,
     obj_name: str,
@@ -33,7 +43,7 @@ def moveTo(
     y_translation_step: float = 0,
     z_translation_step: float = 0,
 ):
-    params = getPositionParams(mitsuba_file)
+    params = getParams(mitsuba_file)
     object = dr.unravel(mi.Point3f, params[f"{obj_name}.vertex_positions"])
     object.x = x_translation_step
     object.y = y_translation_step
@@ -43,4 +53,4 @@ def moveTo(
 
 
 mitsuba_file = "/home/joao/codes/caviar/examples/sionna/simple_street_canyon/simple_street_canyon.xml"
-translate(mitsuba_file, "mesh-Cube", -100, 0, 0)
+translate(mitsuba_file, "mesh-Cube", -741, 0, 0)
