3c4834ae8864f8ff2763ab564d0a047152f95313
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-04-16 15:24:48 -0300

    Added automatic insertion of translate tag on mobile obj XML

--- examples/sionna/obj_move.py
+++ examples/sionna/obj_move.py
@@ -20,16 +20,31 @@ def translate(
 ):
     xml_tree = ET.parse(mitsuba_file)
 
-    # Get the <translate> grandchildren from a <shape> node with the desired "id"
-    translation_element = (
-        xml_tree.getroot()
-        .findall(f"./shape[@id='{obj_name}']/transform/translate")[0]
-        .attrib
-    )
-    # Update the values in <translate> with the desired translation step
-    translation_element[
-        "value"
-    ] = f"{str(x_translation_step)} {str(y_translation_step)} {str(z_translation_step)}"
+    try:
+        # Get the <translate> grandchildren from a <shape> node with the desired "id"
+        translation_element = (
+            xml_tree.getroot()
+            .findall(f"./shape[@id='{obj_name}']/transform/translate")[0]
+            .attrib
+        )
+        # Update the values in <translate> with the desired translation step
+        translation_element[
+            "value"
+        ] = f"{str(x_translation_step)} {str(y_translation_step)} {str(z_translation_step)}"
+
+    except:
+        # In case there is no <translate> grandchildren from a <shape> node with the desired "id"
+        # then get the <shape> node with the desired "id" and add the <translate> grandchildren
+        shape_to_be_translated = xml_tree.getroot().find(f"./shape[@id='{obj_name}']")
+        transform_tag = ET.Element("transform")
+        transform_tag.set("name", "to_world")
+        transform_translate_tag = ET.SubElement(transform_tag, "translate")
+        # Update the values in <translate> with the desired translation step
+        transform_translate_tag.set(
+            "value",
+            f"{str(x_translation_step)} {str(y_translation_step)} {str(z_translation_step)}",
+        )
+        shape_to_be_translated.insert(0, transform_tag)
 
     # Write the modifications to disk
     xml_tree.write(mitsuba_file)
