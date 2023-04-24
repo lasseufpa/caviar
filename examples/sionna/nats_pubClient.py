2dd5b08f8ed6c568c8585a173d493921358d9ee1
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-04-24 08:44:23 -0300

    Formatting NATS example

--- examples/sionna/nats_pubClient.py
+++ examples/sionna/nats_pubClient.py
@@ -16,12 +16,29 @@ with NATSClient() as client:
     def callback(msg):
         print(f"Received a message with subject {msg.subject}: {msg}")
 
-    client.subscribe(subject="caviar.ue.mobility.posotions", callback=callback)
+    client.subscribe(subject="caviar.ue.mobility.positions", callback=callback)
 
-    for i in range(50):
+    for i in range(1):
+        client.publish(
+            subject="caviar.ue.mobility.positions",
+            payload=b"{'UE_type':'UAV','UE_Id':'UAV1','position': {'x':"
+            + str(x1 + i).encode()
+            + b",'y':"
+            + str(y1).encode()
+            + b",'z':"
+            + str(z1).encode()
+            + b"}}",
+        )
 
-         client.publish(subject="caviar.ue.mobility.posotions", payload=b"{'UE_type':'UAV','UE_Id':'UAV1','position': {'x':" + str(x1+i).encode() + b",'y':" + str(y1).encode() + b",'z':" + str(z1).encode() + b"}}")
+        client.publish(
+            subject="caviar.ue.mobility.positions",
+            payload=b"{'UE_type':'UAV','UE_Id':'UAV2','position': {'x':"
+            + str(x2 + i).encode()
+            + b",'y':"
+            + str(y2).encode()
+            + b",'z':"
+            + str(z2).encode()
+            + b"}}",
+        )
 
-         client.publish(subject="caviar.ue.mobility.posotions", payload=b"{'UE_type':'UAV','UE_Id':'UAV2','position': {'x':" + str(x2+i).encode() + b",'y':" + str(y2).encode() + b",'z':" + str(z2).encode() + b"}}")
-
-         client.wait(count=2)
+        client.wait(count=2)
