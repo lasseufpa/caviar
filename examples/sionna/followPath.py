d0a957ce15a0f32ae7d50e4eb976d929d3f1c1af
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-05-02 09:50:35 -0300

    Added AirSim to Sionna position conversion

--- examples/sionna/followPath.py
+++ examples/sionna/followPath.py
@@ -4,6 +4,26 @@ import json
 
 total_ues = 1  # + len(caviar_config.ue_objects)
 
+
+def convertMovementFromAirSimToSionna(airsim_position):
+    """Converts the NED (x: North, y: East, z: Down) coordinates from AirSim
+    into Y-forward, Z-up coordinates for Sionna.
+
+    Args:
+        airsim_position (dict): A dictionary containing the NED
+                                (x: North, y: East, z: Down)
+                                position from AirSim
+
+    Returns:
+        dict: A dictionary containing the Y-forward, Z-up coordinates for Sionna
+    """
+    return {
+        "x": airsim_position["x"],
+        "y": -airsim_position["y"],
+        "z": -airsim_position["z"],
+    }
+
+
 with NATSClient() as natsclient:
     natsclient.connect()
 
@@ -14,11 +34,12 @@ with NATSClient() as natsclient:
             current_step (int): The current step index
         """
         payload = json.loads(msg.payload.decode())
+        sionna_position = convertMovementFromAirSimToSionna(payload["position"])
         run(
             current_step=payload["timestamp"],
-            new_x=payload["position"]["x"],
-            new_y=payload["position"]["y"],
-            new_z=payload["position"]["z"],
+            new_x=sionna_position["x"],
+            new_y=sionna_position["y"],
+            new_z=sionna_position["z"],
         )
         natsclient.wait(count=total_ues)
 
