f955e4f8f1accc8ee390dbf5e9c378b50fc37462
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-06-29 16:55:57 -0300

    Cleaning script

--- examples/sionna/realtime_plot.py
+++ examples/sionna/realtime_plot.py
@@ -2,10 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# Just to set a fixed value for the axes: (xmin, xmax, ymin, ymax)
-# plt.axis([0, 10, 0, 1])
-
 plt.figure()
 plt.title("Throughputs (Gbps)")
 global mean_line1
@@ -16,9 +12,6 @@ mean_line1 = plt.axhline(y=0, color="blue")
 mean_line2 = plt.axhline(y=0, color="green")
 mean_line3 = plt.axhline(y=0, color="red")
 
-# Inspired by Velimir Mlaker answer available at
-# https://stackoverflow.com/questions/11874767/how-do-i-plot-in-real-time-in-a-while-loop
-# CC BY-SA 4.0
 def plot_throughput(timestamp, throughput1, throughput2, throughput3, mean1, mean2, mean3, mean_line1=mean_line1, mean_line2=mean_line2, mean_line3=mean_line3):
     plt.scatter(timestamp, throughput1, color="blue")
     plt.scatter(timestamp, throughput2, color="green")
@@ -27,7 +20,7 @@ def plot_throughput(timestamp, throughput1, throughput2, throughput3, mean1, mea
     mean_line2.set_ydata(mean2)
     mean_line3.set_ydata(mean3)
     plt.legend([f"Optimal   | mean: {round(mean1, 2)}", f"Predicted | mean: {round(mean2, 2)}", f"Random   | mean: {round(mean3, 2)}"], loc="upper left")
-    plt.pause(0.05)
+    plt.pause(0.01)
 
 
 if __name__ == "__main__":
