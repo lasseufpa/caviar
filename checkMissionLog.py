224ed2e3c4be35cffbd4068b2251b79c651e181a
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-08-09 10:39:46 -0300

    Adding pose info and improving log message

--- checkMissionLog.py
+++ checkMissionLog.py
@@ -8,13 +8,15 @@ mission_log = np.load(output_filename, allow_pickle=True)
 
 total_mission_time=mission_log["total_mission_time"]
 rescued_targets=mission_log["rescued_targets"]
+simu_pose_of_rescue=mission_log["simu_pose_of_rescue"]
 simu_time_of_rescue=mission_log["simu_time_of_rescue"]
 throughputs_during_rescue=mission_log["throughputs_during_rescue"]
 times_waited_during_rescue=mission_log["times_waited_during_rescue"]
 
 print(f'\n##################### MISSION LOG ###################\n')
-print(f'total_mission_time: {total_mission_time}')
-print(f'rescued_targets: {rescued_targets}')
-print(f'simu_time_of_rescue: {simu_time_of_rescue}')
-print(f'throughputs_during_rescue: {throughputs_during_rescue}')
-print(f'times_waited_during_rescue: {times_waited_during_rescue}\n')
+print(f'total_mission_time: {total_mission_time} seconds')
+print(f'rescued_targets: {rescued_targets} people')
+print(f'simu_pose_of_rescue: \n{simu_pose_of_rescue} coordinates')
+print(f'simu_time_of_rescue: {simu_time_of_rescue} seconds')
+print(f'throughputs_during_rescue: {throughputs_during_rescue} Gbps')
+print(f'times_waited_during_rescue: {times_waited_during_rescue} seconds\n')
