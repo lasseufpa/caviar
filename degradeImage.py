869a9ece07b672ab1fd77f2cc3b3db0b30cc2fe0
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-08-24 15:01:09 -0300

    Fixed the code to degrade image based on dropped packets

---
+++ degradeImage.py
@@ -0,0 +1,37 @@
+from PIL import Image as img
+import numpy as np
+
+
+rng = np.random.default_rng(1)
+
+
+image = img.open("/home/joaoborges/Downloads/airsimtest.png")
+image_bytes = bytearray(image.tobytes())
+
+
+image_size_bytes = len(image_bytes)
+packet_size_bytes = 65536 # Max size of a TCP packet = 64 KiB = 65536 bytes
+total_number_of_packets = image_size_bytes/packet_size_bytes
+if type(total_number_of_packets) == float:
+    total_number_of_packets = int(total_number_of_packets) + 1
+packet_drop_rate = 0.1
+packets_to_drop = int(packet_drop_rate*total_number_of_packets)
+image_packets = np.array_split(image_bytes, total_number_of_packets)
+# Gets different packages indexes to drop
+dropped_package_indexes = rng.choice(total_number_of_packets, packets_to_drop, replace=False)
+
+
+# Zeroes the bytes of the dropped packages
+for idx in dropped_package_indexes:
+    for value_idx, value in enumerate(image_packets[idx]):
+        image_packets[idx][value_idx] = 0
+
+
+# Reconstructs the image
+reconstructed_image_packets = bytes('', 'utf-8')
+for packet in image_packets:
+    reconstructed_image_packets = reconstructed_image_packets + packet.tobytes()
+
+
+image_bytes_saveable = img.frombytes(mode="RGBA", size=image.size, data=reconstructed_image_packets, decoder_name='raw')
+image_bytes_saveable.save("/home/joaoborges/codes/caviar/runs/detect/predict/fromBytes.png")
