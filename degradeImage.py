0e49b8458e6e90d5bb23a19cdfd8ac9ddeb28f9e
JoãoBorges <joaopaulotavaresborges@gmail.com>
2023-08-29 22:20:55 -0300

    Improved pixel loss function

--- degradeImage.py
+++ degradeImage.py
@@ -1,22 +1,30 @@
 from PIL import Image as img
 import numpy as np
 import time
+import cv2
 
 
 rng = np.random.default_rng(1)
-image = img.open("/home/joaoborges/Downloads/airsimtest.png")
+image = img.open("/home/joao/Downloads/airsimtest.png")
+im = cv2.imread("/home/joao/Downloads/airsimtest.png", cv2.IMREAD_UNCHANGED)
+width = 1
+height = 1
+bit_depth = 32
+pixel_size_bytes = (width * height * bit_depth) / 8
 
 
-def dropPacketsFromImage(image, packet_drop_rate, output_folder="/home/joaoborges/Downloads/fromBytes.png", packet_size_bytes=pixel_size_bytes, rng=rng):
-    width = 1
-    height = 1
-    bit_depth = 32
-    pixel_size_bytes = (width * height * bit_depth) / 8
+def dropPacketsFromImage(
+    image,
+    packet_drop_rate,
+    output_folder="/home/joao/Downloads/fromBytes.png",
+    packet_size_bytes=pixel_size_bytes,
+    rng=rng,
+):
     # number_of_pixels_in_tcp_packet = 65536 / pixel_size_bytes
     # packet_size_bytes = pixel_size_bytes
     # packet_size_bytes = 65536  # Max size of a TCP packet = 64 KiB = 65536 bytes
 
-    starting_instant = time.time()
+    # starting_instant = time.time()
     image_bytes = bytearray(image.tobytes())
     image_size_bytes = len(image_bytes)
 
@@ -25,7 +33,7 @@ def dropPacketsFromImage(image, packet_drop_rate, output_folder="/home/joaoborge
 
     total_number_of_packets = image_size_bytes // int(packet_size_bytes)
     packets_to_drop = int(packet_drop_rate * total_number_of_packets)
-    image_packets = np.array(image_bytes).reshape((total_number_of_packets,-1))
+    image_packets = np.array(image_bytes).reshape((total_number_of_packets, -1))
     # Gets different packages indexes to drop
     dropped_package_indexes = rng.choice(
         total_number_of_packets, packets_to_drop, replace=False
@@ -35,12 +43,40 @@ def dropPacketsFromImage(image, packet_drop_rate, output_folder="/home/joaoborge
     image_packets[dropped_package_indexes] = 0
 
     reconstructed_image_packets = np.array(image_packets)
-    reconstructed_image_packets = np.concatenate(reconstructed_image_packets).astype("b")
+    reconstructed_image_packets = np.concatenate(reconstructed_image_packets).astype(
+        "b"
+    )
 
     image_bytes_saveable = img.frombytes(
-        mode="RGBA", size=image.size, data=reconstructed_image_packets, decoder_name="raw"
+        mode="RGBA",
+        size=image.size,
+        data=reconstructed_image_packets,
+        decoder_name="raw",
     )
 
     image_bytes_saveable.save(output_folder)
-    ending_instant = time.time()
-    print(f"Duration: {ending_instant-starting_instant}")
+    # ending_instant = time.time()
+    # print(f"Duration: {ending_instant-starting_instant}")
+
+
+dropPacketsFromImage(image, 0.25)
+
+
+def applyFilter(
+    image,
+    packet_drop_rate,
+    output_folder="/home/joao/Downloads/fromBytes.png",
+    rng=rng,
+):
+    height = image.shape[0]
+    width = image.shape[1]
+    total_number_of_pixels = height * width * 4
+    packets_to_drop = int(total_number_of_pixels * packet_drop_rate)
+    dropped_package_indexes = rng.choice(
+        total_number_of_pixels, packets_to_drop, replace=False
+    )
+    random_drop_kernel = np.ones(total_number_of_pixels)
+    random_drop_kernel[dropped_package_indexes] = 0
+    random_drop_kernel = random_drop_kernel.reshape((height, width, 4))
+    degraded_image = np.multiply(image, random_drop_kernel).astype("uint8")
+    cv2.imwrite(output_folder, degraded_image)
--- psnr_test.py
+++ psnr_test.py
@@ -2,43 +2,66 @@ import numpy as np
 import cv2
 import time
 from PIL import Image as img
-from degradeImage import dropPacketsFromImage
+from degradeImage import dropPacketsFromImage, applyFilter
 
-from ultralytics import YOLO
-model = YOLO("yolov8n.pt")
+# from ultralytics import YOLO
 
-# image = cv2.imread("/home/joaoborges/Downloads/airsimtest.png")
-# gaussian_noise = np.zeros(np.array(image).shape, np.uint8)
-# cv2.randn(gaussian_noise, 0, 270) 
+# model = YOLO("yolov8n.pt")
+
+init_1 = time.time()
+image = cv2.imread("/home/joao/Downloads/airsimtest.png", cv2.IMREAD_UNCHANGED)
+gaussian_noise = np.zeros(np.array(image).shape, np.uint8)
+# cv2.randn(gaussian_noise, 0, 270)
 # gaussian_noise = gaussian_noise-100 # PSNR: 8.1041 dB
-# cv2.randn(gaussian_noise, 0, 270) # PSNR: 15.8578 dB
+cv2.randn(gaussian_noise, 0, 270)  # PSNR: 15.8578 dB
 # cv2.randn(gaussian_noise, 0, 90) # PSNR: 19.1642 dB
 # cv2.randn(gaussian_noise, 0, 45) # PSNR: 23.585 dB
 # cv2.randn(gaussian_noise, 0, 35) # PSNR: 25.2878 dB
 # cv2.randn(gaussian_noise, 0, 30) # PSNR: 26.5537 dB
 # cv2.randn(gaussian_noise, 0, 25) # PSNR: 28.0667 dB
 # cv2.randn(gaussian_noise, 0, 10) # PSNR: 35.9116 dB
-# degraded_image = cv2.add(image, gaussian_noise)
-# cv2.imwrite("degraded_image.jpg", degraded_image)
+degraded_image = cv2.add(image, gaussian_noise)
+cv2.imwrite("/home/joao/Downloads/degraded_image.png", degraded_image)
+end_1 = time.time()
+################################################################################
 
-image = img.open("/home/joaoborges/Downloads/airsimtest.png")
+init_2 = time.time()
+image2 = img.open("/home/joao/Downloads/airsimtest.png")
 
-# dropPacketsFromImage(image, 0.01)# PSNR: 26.3629 dB | Human detected probability: 0.48686936497688293
-# dropPacketsFromImage(image, 0.1) # PSNR: 16.3643 dB | Human detected probability: 0.5403681993484497
-# dropPacketsFromImage(image, 0.25)# PSNR: 12.3902 dB | Human detected probability: 0.42502260208129883
-# dropPacketsFromImage(image, 0.5)# PSNR: 9.376 dB | Human detected probability: NO DETECTION
+# dropPacketsFromImage(
+#     image, 0.01
+# )  # PSNR: 26.3629 dB | Human detected probability: 0.48686936497688293
+# dropPacketsFromImage(
+#     image, 0.1
+# )  # PSNR: 16.3643 dB | Human detected probability: 0.5403681993484497
+# dropPacketsFromImage(
+#     image, 0.25
+# )  # PSNR: 12.3902 dB | Human detected probability: 0.42502260208129883
+dropPacketsFromImage(
+    image2, 0.5
+)  # PSNR: 9.376 dB   | Human detected probability: NO DETECTION
+end_2 = time.time()
+# time.sleep(1)
+# image = cv2.imread("/home/joao/Downloads/airsimtest.png", cv2.IMREAD_UNCHANGED)
+# degraded_image = cv2.imread("/home/joao/Downloads/fromBytes.png", cv2.IMREAD_UNCHANGED)
+# psnr = cv2.PSNR(image, degraded_image)
 
-time.sleep(1)
-image = cv2.imread("/home/joaoborges/Downloads/airsimtest.png")
-degraded_image = cv2.imread("/home/joaoborges/Downloads/fromBytes.png")
-psnr = cv2.PSNR(image, degraded_image)
+# print(f"PSNR: {round(psnr, 4)} dB")
 
-print(f'PSNR: {round(psnr, 4)} dB')
+# results = model.predict(
+#     source=degraded_image, classes=0, save=True
+# )  # save predictions as labels
 
-results = model.predict(source=degraded_image, classes=0, save=True)  # save predictions as labels
+# try:
+#     print(f"Detected class: {results[0].boxes.data[0,5]}")
+#     print(f"Human detected probability: {results[0].boxes.data[0,4]}")
+# except:
+#     print("NO DETECTION")
 
-try:
-    print(f'Detected class: {results[0].boxes.data[0,5]}')
-    print(f'Human detected probability: {results[0].boxes.data[0,4]}')
-except:
-    print("NO DETECTION")
+init_3 = time.time()
+applyFilter(image, 0.5)
+end_3 = time.time()
+
+print(f"openCV_Noise = {(end_1-init_1) * 1e3} ms")
+print(f"pixel_drop = {(end_2-init_2) * 1e3} ms")
+print(f"pixel_filter = {(end_3-init_3) * 1e3} ms")
