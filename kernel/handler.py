1895681bfb866e871bf7957fc324a9566fdf37d5
João Albuquerque <joao.barbosa.albuquerque@itec.ufpa.br>
2025-02-02 09:36:02 -0300

    kernel: Add __destroy method for general subprocess-killing dealing

--- kernel/handler.py
+++ kernel/handler.py
@@ -1,8 +1,19 @@
-from .logger import LOGGER
+import signal
+import sys
+import threading
 from functools import wraps
+
+from .logger import LOGGER
+from .process import PROCESS
+
+SHT = threading.Event()
 
 
 def exception_handler(func):
+    """
+    This decorator handles exceptions (errors) in the functions.
+    """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
@@ -10,7 +21,37 @@ def exception_handler(func):
             return func(*args, **kwargs)
         except Exception as e:
             LOGGER.error(f"An error occurred in {func.__name__}: {e}")
-            # Optionally, re-raise the exception if you want to propagate it
-            # raise
+            SHT.set()
+            __destroy()
+            sys.exit(1)
 
     return wrapper
+
+
+def __signal_handler(sig, frame):
+    """
+    This method handles the signal interrupt.
+    """
+    LOGGER.warning(f"Signal {sig} received")
+    LOGGER.warning("Interrupt received, shutting down...")
+    __destroy()
+    sys.exit(1)
+
+
+def __destroy():
+    """
+    This method destroys all threads.
+    """
+    PROCESS.kill_processes()
+    LOGGER.debug("Destroying threads and subprocesses")
+    for thread in threading.enumerate():
+        if thread is not threading.current_thread():
+            LOGGER.debug(f"Joining thread {thread.name}")
+            thread.join()
+            if thread.is_alive():
+                LOGGER.error(f"Thread {thread.name} did not terminate")
+    LOGGER.debug("All threads terminated")
+    sys.exit()
+
+
+signal.signal(signal.SIGINT, __signal_handler)
