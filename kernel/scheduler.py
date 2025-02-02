5069e3e12cf914db0408faec058529a6101634f3
João Albuquerque <joao.barbosa.albuquerque@itec.ufpa.br>
2025-02-02 09:39:08 -0300

    kernel: add scheduler class for scheduling logic

---
+++ kernel/scheduler.py
@@ -0,0 +1,25 @@
+import time
+
+
+class scheduler:
+    """
+    This class represents the scheduler for the simulation.
+    """
+
+    def __init__(self):
+        """
+        Constructor that initializes the Scheduler object.
+        """
+        pass
+
+    def set_time_type(self, time_type):
+        """
+        This method sets the time type for the simulation.
+        """
+        self.time_type = time_type
+
+    def set_sync_type(self, sync):
+        """
+        This method sets the synchronization type for the simulation.
+        """
+        self.sync = sync
