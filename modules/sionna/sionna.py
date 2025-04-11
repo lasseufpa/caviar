8c64befbbe690a79f477fa950023669545fa4843
João Albuquerque <joao.barbosa.albuquerque@itec.ufpa.br>
2025-04-11 09:08:38 -0300

    modules: (sionna) Start refactoring sionna to 1.0.2

--- modules/sionna/sionna.py
+++ modules/sionna/sionna.py
@@ -1,16 +1,19 @@
 from kernel.logger import LOGGER
 from kernel.module import module
 
+import sionna.rt as RT
 
 class sionna(module):
     """
     The Sionna module is the class that handles all the Sionna setup
     """
-
+    
     def _do_init(self):
         """
         This method initializes all the necessary Sionna configuration.
         """
+        #RT.load_scene()
+        #RT.PathSolver()
         LOGGER.debug(f"Sionna Do Init")
 
     async def _execute_step(self):
