693b688d9531435f9fa50292c51749610cd13356
João Albuquerque <joao.barbosa.albuquerque@itec.ufpa.br>
2025-04-11 08:49:22 -0300

    kernel: (temp) Decrease inter-step inerval

--- kernel/scheduler.py
+++ kernel/scheduler.py
@@ -53,7 +53,7 @@ class Scheduler(ABC):
         while True:
             LOGGER.debug(f"Event_id: {self._event}")
             self._execute_step()
-            self.__wait(15)
+            self.__wait(.1)
             # if self._stop_condition():
             #    break
             self._event += 1
